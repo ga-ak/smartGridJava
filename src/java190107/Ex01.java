8929c61e1b937d378cebb51839705d98840f379e
cheolho <gghg0000@gmail.com>
2019-01-07 16:36:40 +0900

    깃허브를 이용한 스마트그리드 수업 소스 코드관리

---
+++ src/java190107/Ex01.java
@@ -0,0 +1,30 @@
+package java190107;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        // 한번에 2차원 배열 선언
+        int sum = 0;
+        int[][] total = new int[3][5];
+        int[] answer = {4, 5, 4, 1, 2};
+        int[] score = {10, 20, 30, 20, 20};
+        total[0] = answer;
+        total[2] = score;
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<total[1].length; i++) {
+            total[1][i] = sc.nextInt();
+        }
+
+        for(int i=0; i<total[1].length; i++) {
+            if(total[0][i] == total[1][i]) {
+                sum += total[2][i];
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
