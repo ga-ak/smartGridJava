a5d4446c7ae5ba04958f8cb7f9b64eec72513fe3
cheolho <gghg0000@gmail.com>
2019-01-08 12:11:44 +0900

    깃허브를 이용한 스마트그리드 수업 소스 코드관리

--- src/java190107/Ex03.java
+++ src/java190107/Ex03.java
@@ -1,24 +1,23 @@
 package java190107;
 
-public class Ex02 {
+public class Ex03 {
     public static void main(String[] args) {
         int[][] intArray = new int[5][5];
         int count = 21;
         for(int i=0; i<intArray.length; i++) {
             for(int j=0; j<intArray[i].length; j++) {
-                intArray[i][j] = count++;
+                if(i%2 ==0) {
+                    intArray[i][j] = count++;
+                }else if (i%2 !=0) {
+                   intArray[i][intArray[i].length-j-1] = count++;
+                }
             }
         }
 
         for(int i=0; i<intArray.length; i++) {
             System.out.print("intArray["+i+"] >> ");
-            for(int j=0; j<intArray[i].length; j++) {
-                if(i%2==0) {
-                    System.out.print(intArray[i][j]);
-                } else if(i%2!=0) {
-                    System.out.print(intArray[i][intArray.length-1-j]);
-                }
-                System.out.print("\t");
+            for(int j=0; j<intArray[i].length; j++){
+                System.out.print(intArray[i][j]+"\t");
             }
             System.out.println();
         }
---
+++ src/java_festival/P13.java
@@ -0,0 +1,4 @@
+package java_festival;
+
+public class P13 {
+}
