6de51981967f8513173a4c2c8697c75887c8e539
cheolho <gghg0000@gmail.com>
2019-01-09 16:50:15 +0900

    19-01-09 cgi

--- src/temp/Temp.java
+++ src/temp/Temp.java
@@ -5,7 +5,10 @@ import java.util.StringTokenizer;
 
 public class Temp {
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
+        char a = 'a';
+        char b = 'b';
+
+        System.out.println();
 
 
 
